564274c43d626cb67f6231a3bf53193bb4e5c572
Kyle Sayers <kylesayrs@gmail.com>
2024-06-14 15:37:38 -0400

    Implemented node registration

--- twoPhaseCommit/src/main/java/com/ksayers/twoPhaseCommit/CoordinatorNode.java
+++ twoPhaseCommit/src/main/java/com/ksayers/twoPhaseCommit/CoordinatorNode.java
@@ -2,17 +2,21 @@ package com.ksayers.twoPhaseCommit;
 
 import java.util.HashMap;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.Executors;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
 
 
 public class CoordinatorNode {
-    HashMap<String, String> nodeList = new HashMap<String, String>();
+    HashMap<String, InetSocketAddress> nodeList = new HashMap<String, InetSocketAddress>();
     ThreadPoolExecutor serverThreadPool = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
     HttpServer server;
 
@@ -30,9 +34,13 @@ public class CoordinatorNode {
     public static void main(String[] args) throws IOException {
         System.out.println("main");
 
+        @SuppressWarnings("unused")
         CoordinatorNode node = new CoordinatorNode();
     }
 
+    /*
+     * Handle register requests
+     */
     private class CoordinatorNodeHttpHandler implements HttpHandler {
         @Override    
         public void handle(HttpExchange httpExchange) {
@@ -42,29 +50,57 @@ public class CoordinatorNode {
                     return;
                 }
 
-                sendErrorResponse(httpExchange);
-
-                if (false) {
-                    throw new IOException();
-                }
+                registerNode(httpExchange);
             
-            } catch (IOException exception) {
+            } catch (IOException | JSONException exception) {
                 exception.printStackTrace();
                 sendErrorResponse(httpExchange);
-                //httpExchange.getRequestMethod()
             }
         }
 
+        private void registerNode(HttpExchange httpExchange) throws IOException {
+            // parse request body
+            InputStream requestBodyStream = httpExchange.getRequestBody();
+            byte[] requestBodyBytes = requestBodyStream.readNBytes(100);
+            String requestBody = new String(requestBodyBytes);
+            JSONObject requestJson;
+            try {
+                requestJson = new JSONObject(requestBody);
+            }
+            catch (JSONException exception) {
+                exception.printStackTrace();
+                sendErrorResponse(httpExchange);
+                return;
+            }
+
+            // input validation
+            if (!requestJson.has("nodeId") || !requestJson.has("address") || !requestJson.has("port")) {
+                sendErrorResponse(httpExchange);
+                return;
+            }
+
+            // update node list
+            nodeList.put(
+                requestJson.getString("nodeId"),
+                new InetSocketAddress(requestJson.getString("address"), requestJson.getInt("port"))
+            );
+            System.out.println(nodeList);
+
+            // send okay response
+            OutputStream outputStream = httpExchange.getResponseBody();
+            httpExchange.sendResponseHeaders(200, 0);
+            outputStream.flush();
+            outputStream.close();
+        }
+
         private void sendErrorResponse(HttpExchange httpExchange) {
-            System.err.println("Error response");
             OutputStream outputStream = httpExchange.getResponseBody();
             try {
                 httpExchange.sendResponseHeaders(400, 0);
                 outputStream.flush();
                 outputStream.close();
             } catch (IOException exception) {
-                exception.printStackTrace();
-                System.err.println("Failed to send error response");
+                exception.printStackTrace();;
             }
         }
     }
