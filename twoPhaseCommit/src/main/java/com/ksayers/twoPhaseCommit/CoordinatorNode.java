ce40b233f64fbabab18a8c228c569f5ed38e47a8
Kyle Sayers <kylesayrs@gmail.com>
2024-06-15 00:21:42 -0400

    Implement transaction logic, needs testing

--- twoPhaseCommit/src/main/java/com/ksayers/twoPhaseCommit/CoordinatorNode.java
+++ twoPhaseCommit/src/main/java/com/ksayers/twoPhaseCommit/CoordinatorNode.java
@@ -2,7 +2,6 @@ package com.ksayers.twoPhaseCommit;
 
 import java.util.Map;
 import java.util.Scanner;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.io.IOException;
 import java.io.InputStream;
@@ -39,72 +38,94 @@ public class CoordinatorNode implements AutoCloseable {
     }
 
     public boolean doTransaction(String message) {
-        Pair<ArrayList<Integer>, ArrayList<String>> readyResponses = sendMessageToNodes("ready", message);
+        // perform ready phase
+        Map<String, Pair<Integer, String>> readyResponses = sendMessageToNodes("ready", message, false);
         System.out.println(readyResponses);
 
-        return true;
-    }
-
-    /**
-     * Sends a message to all nodes in the nodeList.
-     * 
-     * @param path The path to send the message to.
-     * @param message The message to send.
-     * @return A Pair object containing the response codes and response bodies from each node.
-     */
-    private Pair<ArrayList<Integer>, ArrayList<String>> sendMessageToNodes(String path, String message) {
-        ArrayList<Integer> responseCodes = new ArrayList<Integer>();
-        ArrayList<String> responseBodies = new ArrayList<String>();
-
-        for (Map.Entry<String, InetSocketAddress> entry : nodeList.entrySet()) {
-            String nodeId = entry.getKey();
-            InetSocketAddress address = entry.getValue();
-            
-            try {
-                URL url = new URI(String.format(
-                    "http://%s:%s/%s",
-                    address.getHostName(),
-                    address.getPort(),
-                    path
-                )).toURL();
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("PUT");
-                connection.setDoOutput(true);
-                
-                // send commit message
-                OutputStream outputStream = connection.getOutputStream();
-                outputStream.write(message.getBytes());
-                outputStream.flush();
-                outputStream.close();
-                
-                // check response code
-                int responseCode = connection.getResponseCode();
-                if (responseCode == HttpURLConnection.HTTP_OK) {
-                    System.out.println(String.format("Commit message sent to node %s at $s", nodeId, url));
-                } else {
-                    System.err.println(String.format("Failed to send commit message node %s at $s", nodeId, url));
-                }
-
-                // get response body
-                InputStream inputStream = connection.getInputStream();
-                byte[] responseBodyBytes = inputStream.readAllBytes();
-                String responseBody = new String(responseBodyBytes);
-                
-                connection.disconnect();
-
-                responseCodes.add(responseCode);
-                responseBodies.add(responseBody);
-
-            } catch (Exception exception) {
-                System.err.println(String.format("Failed to send % message node %s", path, nodeId));
-                exception.printStackTrace();
-
-                responseCodes.add(null);
-                responseBodies.add(null);
+        // check if any nodes are not ready
+        boolean readySuccess = true;
+        for (Map.Entry<String, Pair<Integer, String>> entry : readyResponses.entrySet()) {
+            Integer responseCode = entry.getValue().first;
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                readySuccess = false;
+                break;
             }
         }
 
-        return new Pair<ArrayList<Integer>, ArrayList<String>>(responseCodes, responseBodies);
+        // if not all nodes are ready, abort
+        if (!readySuccess) {
+            System.out.println("Not all nodes are ready, abort");
+            sendMessageToNodes("abort", message, true);
+            return false;
+        }
+
+        // perform commit phase
+        sendMessageToNodes("commit", message, true);
+        return true;
+    }
+
+    private Map<String, Pair<Integer, String>> sendMessageToNodes(String path, String message, boolean retryForever) {
+        Map<String, Pair<Integer, String>> responses = new HashMap<String, Pair<Integer, String>>();
+        for (Map.Entry<String, InetSocketAddress> entry : nodeList.entrySet()) {
+            String nodeId = entry.getKey();
+            
+            Pair<Integer, String> nodeResponse = sendMessageToNode(nodeId, path, message);
+            Integer responseCode = nodeResponse.first;
+            while (retryForever && responseCode != HttpURLConnection.HTTP_OK) {
+                System.err.println(String.format("Failed to send %s message to node %s", path, nodeId));
+                nodeResponse = sendMessageToNode(nodeId, path, message);
+                responseCode = nodeResponse.first;
+            }
+
+            responses.put(nodeId, nodeResponse);
+        }
+
+        return responses;
+    }
+
+    private Pair<Integer, String> sendMessageToNode(String nodeId, String path, String message) {
+        InetSocketAddress address = nodeList.get(nodeId);
+
+        try {
+            URL url = new URI(String.format(
+                "http://%s:%s/%s",
+                address.getHostName(),
+                address.getPort(),
+                path
+            )).toURL();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("PUT");
+            connection.setDoOutput(true);
+            
+            // send commit message
+            OutputStream outputStream = connection.getOutputStream();
+            outputStream.write(message.getBytes());
+            outputStream.flush();
+            outputStream.close();
+            
+            // check response code
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                System.out.println(String.format("Commit message sent to node %s at $s", nodeId, url));
+            } else {
+                System.err.println(String.format("Failed to send commit message node %s at $s", nodeId, url));
+            }
+
+            // get response body
+            InputStream inputStream = connection.getInputStream();
+            byte[] responseBodyBytes = inputStream.readAllBytes();
+            String responseBody = new String(responseBodyBytes);
+            
+            connection.disconnect();
+
+            return new Pair<Integer,String>(responseCode, responseBody);
+
+        } catch (Exception exception) {
+            System.err.println(String.format("Failed to send % message node %s", path, nodeId));
+            exception.printStackTrace();
+
+            return new Pair<Integer,String>(null, null);
+        }
     }
 
     public static void main(String[] args) throws IOException {
